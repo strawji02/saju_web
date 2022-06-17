2c8eb1a82079cacf111781dcf3ca90091d0f41d6
황준혁 <strawji02@naver.com>
2022-06-17 09:53:58 +0900

    feat: init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://175.196.233.60:80/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
