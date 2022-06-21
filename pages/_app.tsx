1da96d54ce90a1048eae9a144c971a0a9afe41df
황준혁 <strawji02@naver.com>
2022-06-21 12:35:58 +0900

    feat: :sparkles: add react-qurey client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,41 +3,45 @@ import Head from 'next/head';
 import { Center, Container, MantineProvider } from '@mantine/core';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const queryClient = new QueryClient();
 
   return (
     <>
-      <Head>
-        <title>간산 철학원</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'dark',
-          fontFamily: 'Open Sans, sans serif',
-        }}
-      >
-        <Center style={{ height: '100vh', backgroundColor: '#333333' }}>
-          <Container
-            style={{
-              height: '100%',
-              // backgroundColor: '#ffffff',
-              maxWidth: 350,
-              width: 350,
-            }}
-          >
-            <Component {...pageProps} />
-          </Container>
-        </Center>
-      </MantineProvider>{' '}
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>간산 철학원</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: 'dark',
+            fontFamily: 'Open Sans, sans serif',
+          }}
+        >
+          <Center style={{ height: '100vh', backgroundColor: '#333333' }}>
+            <Container
+              style={{
+                height: '100%',
+                // backgroundColor: '#ffffff',
+                maxWidth: 350,
+                width: 350,
+              }}
+            >
+              <Component {...pageProps} />
+            </Container>
+          </Center>
+        </MantineProvider>
+      </QueryClientProvider>
     </>
   );
 }
