313f06b4f3c675c34495d5c27dba84b6f501f5ef
황준혁 <strawji02@naver.com>
2022-06-21 12:36:55 +0900

    feat: :sparkles: add zustand store

--- store/store.ts
+++ store/store.ts
@@ -1,48 +1,87 @@
 import create from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools, persist, subscribeWithSelector } from 'zustand/middleware';
 
-type Name = string;
-type Gender = number;
-type Birthplace = string;
-type Calendar = 'lunar' | 'solar';
-type Birth = Date;
-type Intercalation = boolean;
+export type Name = string;
+export type Gender = number;
+export type Birthplace = string;
+export type Calendar = 'lunar' | 'solar';
+export type BirthDate = Date;
+export type Intercalation = boolean;
 
+export interface Birth {
+  birthplace: Birthplace;
+  calendar: Calendar;
+  birthDay: BirthDate;
+  birthHour: BirthDate;
+  intercalation: Intercalation;
+}
 interface UserState {
   name: Name;
   setName: (name: Name) => void;
   gender: Gender; //1 or 2
   setGender: (gender: Gender) => void;
-  birthplace: Birthplace;
-  setBirthplace: (birthplace: Birthplace) => void;
-  calendar: Calendar;
-  setCalendar: (calendar: Calendar) => void;
   birth: Birth;
   setBirth: (birth: Birth) => void;
-  intercalation: Intercalation;
-  setIntercalation: (intercalation: Intercalation) => void;
+}
+
+export interface SajuData {
+  lunar: string;
+  lunar_ss: string;
+  solar: string;
+  year: string;
+  month: string;
+  day: string;
+  time: string;
+}
+interface SajuState {
+  data: SajuData;
+  setSaju: (data: SajuData) => void;
 }
 
 export const useUserState = create<UserState>()(
   devtools(
-    persist((set) => ({
-      name: '',
-      setName: (name) => set((state) => ({ name })),
-      gender: 1,
-      setGender: (gender) => set((state) => ({ gender })),
-      birthplace: '',
-      setBirthplace: (birthplace) => set((state) => ({ birthplace })),
-      calendar: 'solar',
-      setCalendar: (calendar) => set((state) => ({ calendar })),
-      birth: new Date(),
-      setBirth: (birth) => set((state) => ({ birth })),
-      intercalation: false,
-      setIntercalation: (intercalation) =>
-        set((state) =>
-          state.calendar === 'lunar'
-            ? { intercalation }
-            : { intercalation: false }
-        ),
-    }))
+    persist(
+      subscribeWithSelector((set) => ({
+        name: '',
+        setName: (name) => set(() => ({ name })),
+        gender: 1,
+        setGender: (gender) => set(() => ({ gender })),
+        birth: {
+          birthplace: '',
+          calendar: 'solar',
+          birthDay: new Date(),
+          birthHour: new Date(),
+          intercalation: false,
+        },
+        setBirth: (birth) => set(() => ({ birth })),
+      })),
+      {
+        name: 'user-storage',
+        getStorage: () => sessionStorage,
+      }
+    )
+  )
+);
+
+export const useSajuState = create<SajuState>()(
+  devtools(
+    persist(
+      subscribeWithSelector((set) => ({
+        data: {
+          lunar: '',
+          lunar_ss: '',
+          solar: '',
+          year: '',
+          month: '',
+          day: '',
+          time: '',
+        },
+        setSaju: (data) => set(() => ({ data })),
+      })),
+      {
+        name: 'saju-storage',
+        getStorage: () => sessionStorage,
+      }
+    )
   )
 );
