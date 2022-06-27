e7a85442fbe4c1343cee2e772720fffc1b1e785b
황준혁 <strawji02@naver.com>
2022-06-27 17:37:41 +0900

    build

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -45,6 +45,27 @@ function App(props) {
                         /*#__PURE__*/ jsx_runtime_.jsx("meta", {
                             name: "viewport",
                             content: "minimum-scale=1, initial-scale=1, width=device-width"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                            rel: "shortcut icon",
+                            href: "/images/favicon.ico"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                            rel: "apple-touch-icon",
+                            sizes: "180x180",
+                            href: "/images/apple-touch-icon.png"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                            rel: "icon",
+                            type: "image/png",
+                            sizes: "32x32",
+                            href: "/images/favicon-32x32.png"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                            rel: "icon",
+                            type: "image/png",
+                            sizes: "16x16",
+                            href: "/images/favicon-16x16.png"
                         })
                     ]
                 }),
