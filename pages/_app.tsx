0c78edba26548bf35a291fe2c31b5c922790f6e3
황준혁 <strawji02@naver.com>
2022-06-27 17:35:42 +0900

    feat: :lipstick: favicon add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,24 @@ export default function App(props: AppProps) {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
           />
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
         </Head>
         <MantineProvider
           withGlobalStyles
