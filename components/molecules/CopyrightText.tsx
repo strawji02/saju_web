8f40417fdc8d0d74250eae0eb26d20275e0aafdc
황준혁 <strawji02@naver.com>
2022-12-03 18:17:45 +0900

    feat: :sparkles: add main page atoms & molecules

---
+++ components/molecules/CopyrightText.tsx
@@ -0,0 +1,17 @@
+import { Stack, Text } from '@mantine/core';
+import React from 'react';
+
+function CopyrightText() {
+  return (
+    <Stack mt="lg" mb="lg" spacing={0} align="center">
+      <Text style={{ fontSize: 10 }} color="gray">
+        이메일 : webmaser@gmail.com
+      </Text>
+      <Text style={{ fontSize: 10 }} color="gray">
+        Copyright ⓒ All Rights Reserved.
+      </Text>
+    </Stack>
+  );
+}
+
+export default CopyrightText;
