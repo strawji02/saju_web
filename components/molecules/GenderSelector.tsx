3426e9ceae32beba996c59d6a5f7c00b495487bd
황준혁 <strawji02@naver.com>
2022-12-08 10:51:01 +0900

    feat: :sparkles: add gender selector & next button atoms/molecules

--- components/molecules/GenderSelector.tsx
+++ components/molecules/GenderSelector.tsx
@@ -1,13 +1,28 @@
 import { Group } from '@mantine/core';
-import { useState } from 'react';
+import { UseFormReturnType } from '@mantine/form';
+import { useEffect, useState } from 'react';
 import GenderSelectButton from '../atoms/GenderSelectButton';
-import { Gender } from '../types/StepInput';
+import { Gender, StepInputFormType } from '../types/StepInput';
 
-function GenderSelector() {
-  const [gender, setGender] = useState<Gender | undefined>(undefined);
+interface Props {
+  form: UseFormReturnType<
+    StepInputFormType,
+    (values: StepInputFormType) => StepInputFormType
+  >;
+}
+
+function GenderSelector({ form }: Props) {
+  const [gender, setGender] = useState<Gender | undefined>(form.values.gender);
+
+  useEffect(() => {
+    if (gender) {
+      form.setValues({ gender });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gender]);
 
   return (
-    <Group w={'100%'} position="center" spacing="xl">
+    <Group w={'100%'} position="center">
       <GenderSelectButton
         onClick={() => setGender('남자')}
         selected={gender === '남자'}
