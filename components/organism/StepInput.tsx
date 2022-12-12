535f1ee65cc76413f1ac7bf1bb0677a5d465ef93
황준혁 <strawji02@naver.com>
2022-12-12 17:19:47 +0900

    perf: :zap: update result logic

--- components/organism/StepInput.tsx
+++ components/organism/StepInput.tsx
@@ -1,20 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Box, Stack } from '@mantine/core';
 import { UseFormReturnType } from '@mantine/form';
-import { useRouter } from 'next/router';
-import { useMutation, useQuery } from 'react-query';
 import { UrlObject } from 'url';
-import { getResult } from '../api/result';
 import NextButton from '../atoms/NextButton';
 import StepTitle from '../molecules/StepTitle';
-import { useUserResultState } from '../../utils/state';
-import {
-  ResultParams,
-  StepInputFormType,
-  StepTitleType,
-} from '../types/StepInput';
-import Form from './Form';
-import { PropsWithChildren, useEffect } from 'react';
+import { StepInputFormType, StepTitleType } from '../types/StepInput';
+import { PropsWithChildren } from 'react';
 
 interface Props {
   href: string | UrlObject;
@@ -33,32 +24,6 @@ function StepInput({
   form,
   children,
 }: PropsWithChildren<Props>) {
-  const router = useRouter();
-  const { setResult } = useUserResultState();
-
-  const params = {
-    gender: form.values.gender || '',
-    birthplace: form.values.birthPlace,
-    calendar: form.values.calendar ? '음력' : '양력',
-    year: form.values.birthDate.getFullYear(),
-    month: form.values.birthDate.getMonth() + 1,
-    day: form.values.birthDate.getDate(),
-    hour: form.values.birthTime.getHours(),
-    min: form.values.birthTime.getMinutes(),
-    intercalation: form.values.intercalation ? '윤달' : null,
-  };
-
-  const resultMutate = useMutation(
-    'get-result',
-    async (params: ResultParams) => getResult(params),
-    {
-      onSuccess: (data) => {
-        setResult(data);
-        router.push('/result');
-      },
-    }
-  );
-
   const isLastStep = step === 4;
 
   return (
@@ -69,12 +34,10 @@ function StepInput({
       </Box>
       <NextButton
         onClick={(e) => {
-          if (isLastStep) resultMutate.mutate(params);
           if (form.validate().hasErrors) e.preventDefault();
         }}
         disabled={!form.isValid()}
-        href={isLastStep ? { query: { step: 4 } } : href}
-        isLoading={resultMutate.isLoading}
+        href={isLastStep ? '/result' : href}
         btnText={isLastStep ? '결과보기' : '다음'}
       />
     </Stack>
--- pages/result.tsx
+++ pages/result.tsx
@@ -1,28 +1,61 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Modal } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
+import { getResult } from '../components/api/result';
+import Loading from '../components/atoms/Loading';
 import ResTemplate from '../components/template/ResTemplate';
+import { ResultParams } from '../components/types/StepInput';
 import { useUserResultState } from '../utils/state';
 
 function Result() {
   const router = useRouter();
-  const { result, removeResult, userData, removeUserData } =
-    useUserResultState();
+  const { userData } = useUserResultState();
+
+  const [params, setParams] = useState<ResultParams>();
+
+  const resultMutate = useMutation(
+    'get-result',
+    async (params: ResultParams) => getResult(params),
+    {}
+  );
 
   useEffect(() => {
-    if (!result) router.replace('/');
-  }, [result]);
+    if (userData) {
+      setParams({
+        gender: userData.gender || '',
+        birthplace: userData.birthPlace,
+        calendar: userData.calendar ? '음력' : '양력',
+        year: new Date(userData.birthDate || '0').getFullYear(),
+        month: new Date(userData.birthDate || '0').getMonth() + 1,
+        day: new Date(userData.birthDate || '0').getDate(),
+        hour: new Date(userData.birthTime || '0').getHours(),
+        min: new Date(userData.birthTime || '0').getMinutes(),
+        intercalation: userData.intercalation ? '윤달' : null,
+      });
+    } else {
+      router.push('/');
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    if (params) resultMutate.mutate(params);
+  }, [params]);
 
   return (
     <Modal
       fullScreen
       opened={true}
       onClose={() => {
-        removeResult();
+        router.push('/');
       }}
     >
-      <ResTemplate result={result} userData={userData} />
+      {resultMutate.data ? (
+        <ResTemplate result={resultMutate.data} userData={userData} />
+      ) : (
+        <Loading />
+      )}
     </Modal>
   );
 }
