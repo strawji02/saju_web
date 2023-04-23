85a52e26e88b31bb99624c8f609df2ffb617e8a0
strawji <strawji02@naver.com>
2023-04-23 17:47:55 +0900

    feat: :sparkles: 나와 맞는/안맞는 일주에 대한 자세한 설명을 추가했습니다

--- components/organism/IljuModalContents.tsx
+++ components/organism/IljuModalContents.tsx
@@ -1,13 +1,26 @@
-import { Stack } from '@mantine/core';
+import { Skeleton, Stack } from '@mantine/core';
 import Title from '../atoms/Title';
 import { SAJU_60, SAJU_HAN_60 } from '../../utils/utils';
 import UnderLinedText from '../atoms/UnderLinedText';
+import { useQuery } from 'react-query';
+import { getDesctiprion } from '../api/description';
+import MarkDescriptionText from '../molecules/MarkDescriptionText';
 
 export interface IljuModalContentsProps {
   ilju?: number;
 }
 
 function IljuModalContents({ ilju, ...props }: IljuModalContentsProps) {
+  const { data } = useQuery(
+    `get-description-${ilju}`,
+    async () => getDesctiprion(SAJU_60[ilju as number]),
+    {
+      enabled: !!ilju,
+    }
+  );
+
+  console.log(data);
+
   return (
     <Stack spacing={2}>
       <Title fz={20} c="dark">
@@ -22,7 +35,17 @@ function IljuModalContents({ ilju, ...props }: IljuModalContentsProps) {
       <Title fz={20} c="dark">
         사람의 성향과 심리 그리고 운세
       </Title>
-      {`{여기엔 API에서 받아온 일주 설명이 들어갈거에요}`}
+      {data ? (
+        <MarkDescriptionText resultStr={data} />
+      ) : (
+        <Stack>
+          <Skeleton w={'100%'} h={100} />
+          <Skeleton w={'100%'} h={100} />
+          <Skeleton w={'100%'} h={100} />
+          <Skeleton w={'100%'} h={100} />
+          <Skeleton w={'100%'} h={100} />
+        </Stack>
+      )}
     </Stack>
   );
 }
